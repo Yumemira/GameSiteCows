c9dcd5f9cc0248a3ff54b7b4ad0339c6883ab424
Yumemira <surrenderyumi@mail.ru>
2023-02-22 08:56:45 +0300

    starting front

--- game-front/src/App.js
+++ game-front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {SocketContext, socket} from './views/components/socket'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Town from './views/Town.js'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SocketContext.Provider value={socket}>
+      <Router>
+        <div className="app-container">
+          <Routes>
+            <Route path='' key="b" element={<Town socket={socket} />} />
+          </Routes>
+        </div>
+    </Router>
+  </SocketContext.Provider>
+  )
 }
 
-export default App;
+export default App
---
+++ game-front/src/views/Town.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default class Town extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+}
